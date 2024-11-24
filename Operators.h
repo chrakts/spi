faf412ad8fb9fcc0dfbb22f98c4ad78150f0af05
chrak <kaufen@cmja.de>
2024-11-24 08:29:33 +0100

    pragma once eingeführt

--- Operators.h
+++ Operators.h
@@ -1,19 +1,7 @@
-/*
- * IncFile1.h
- *
- * Created: 16.03.2017 15:26:41
- *  Author: a16007
- */ 
-
-
-#ifndef INCFILE1_H_
-#define INCFILE1_H_
+#pragma once
 
 #include <stdlib.h>
 #include <stddef.h>
 
 void * operator new(size_t size);
 void operator delete(void * ptr);
-
-
-#endif /* INCFILE1_H_ */
